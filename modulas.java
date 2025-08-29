96c8960686cbf01b4fd76e19740e69c1d8978aa0
nikhilguptaa0 <nikhilguptaa.mail@gmail.com>
2025-08-29 22:07:16 +0530

    Basic java programs

---
+++ modulas.java
@@ -0,0 +1,9 @@
+public class modulas {
+    public static void main(String[] args){
+        int A = 200;
+        int B = 22;
+        int modulas = A % B;
+        System.out.println(modulas);
+    }
+    
+}
---
+++ multiply.java
@@ -0,0 +1,10 @@
+public class multiply {
+    public static void main(String[] args){
+        int A = 10;
+        int B = 20;
+        int multiply = A * B;
+        System.out.println(multiply);
+    }
+    
+    
+}
